7d8cb01b0e5f82a1960d2d5544403935a38e9188
kajalsharma987 <7409kajal@gmail.com>
2025-05-13 13:27:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function update() {
   // Collision with food
   if (head.x === food.x && head.y === food.y) {
     score++;
-    level *= 2;
+    level *= 1;
     if (soundOn) eatSound.play();
     food = generateFood();
     if (score > highScore) {
